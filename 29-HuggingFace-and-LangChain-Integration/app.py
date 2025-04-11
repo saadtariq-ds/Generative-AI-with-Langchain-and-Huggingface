db3c290185a3a4e3a2926b20a97bcb33c3578b6c
saadtariq-ds <tariqsaad1997@gmail.com>
2025-04-11 17:50:43 +0500

    Text Summarization with HuggingFace Endpoint

---
+++ 29-HuggingFace-and-LangChain-Integration/app.py
@@ -0,0 +1,70 @@
+import validators
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain.chains.summarize import load_summarize_chain
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_community.document_loaders import YoutubeLoader, UnstructuredURLLoader
+
+# Streamlit App
+st.set_page_config(
+    page_title="LangChain: Summarize Text from Website or YouTube",
+    page_icon=":parrot:"
+)
+st.title(":parrot: LangChain: Summarize Text from Website or YouTube")
+st.subheader("Summarize URL")
+
+## Get Groq API Key and URL Field to Summarize
+with st.sidebar:
+    hf_api_key = st.text_input("HuggingFace API Key", value="", type="password")
+
+generic_url = st.text_input("URL", label_visibility="collapsed")
+
+## Setting LLM
+repo_id = "mistralai/Mistral-7B-Instruct-v0.3"
+llm = HuggingFaceEndpoint(
+    repo_id=repo_id,
+    task="text-generation",
+    max_length=150,
+    temperature=0.7,
+    huggingfacehub_api_token=hf_api_key
+)
+
+## Setting Prompt Template
+prompt_template = """ Provide a summary of the following content in 300 words:
+Content: {text}
+"""
+
+prompt = PromptTemplate(
+    template=prompt_template,
+    input_variables=["text"]
+)
+
+if st.button("Summarize the Content"):
+    if not hf_api_key.strip() or not generic_url.strip():
+        st.error("Please provide the Information")
+    elif not validators.url(generic_url):
+        st.error("Please enter a Valid URL from Website or YouTube")
+    else:
+        try:
+            with st.spinner("Waiting..."):
+                if "youtube.com" in generic_url:
+                    loader = YoutubeLoader.from_youtube_url(
+                        youtube_url=generic_url, add_video_info=True
+                    )
+                else:
+                    loader = UnstructuredURLLoader(
+                        urls=[generic_url], ssl_verify=False,
+                        headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36"}
+                    )
+                data = loader.load()
+
+                ## Chain for Summarization
+                chain = load_summarize_chain(
+                    llm=llm, chain_type='stuff', prompt=prompt
+                )
+                output_summary = chain.run(data)
+                st.success(output_summary)
+
+        except Exception as e:
+            st.exception(f"Exception: {e}")
+
