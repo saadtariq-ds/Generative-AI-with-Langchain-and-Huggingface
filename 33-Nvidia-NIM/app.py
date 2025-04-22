e3b5ed032e48aec114e4bacaecb573ac108ea620
saadtariq-ds <tariqsaad1997@gmail.com>
2025-04-22 15:40:07 +0500

    Llama3 with Nvidia

---
+++ 33-Nvidia-NIM/app.py
@@ -0,0 +1,23 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+  base_url = "https://integrate.api.nvidia.com/v1",
+  api_key = os.getenv("NVIDIA_API_KEY")
+)
+
+completion = client.chat.completions.create(
+  model="meta/llama3-70b-instruct",
+  messages=[{"role":"user","content":"Provide me an article on Machine Learning"}],
+  temperature=0.5,
+  top_p=1,
+  max_tokens=1024,
+  stream=True
+)
+
+for chunk in completion:
+  if chunk.choices[0].delta.content is not None:
+    print(chunk.choices[0].delta.content, end="")
